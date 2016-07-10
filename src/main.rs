6d633d77cecfbdbc581b98aa13c6011fcffd259f
Bernd Amend <bernd.amend@gmail.com>
2016-07-10 07:35:36 +0200

    integrate webinterface

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
 extern crate regex;
 extern crate time;
+extern crate hyper;
 
+use std::collections::BTreeMap;
 use std::io::BufReader;
 use std::io::prelude::*;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use regex::Regex;
-use std::fs::OpenOptions;
 
-#[derive(Debug,PartialEq,Eq)]
+use std::sync::{Arc, RwLock};
+use std::thread;
+
+use std::io;
+use std::io::copy;
+use hyper::status::StatusCode;
+use hyper::header::{SetCookie, Cookie, ContentType, ContentLength, HttpDate};
+use hyper::server::{Handler, Server, Request, Response};
+use hyper::{Get, Post};
+use hyper::uri::RequestUri::AbsolutePath;
+use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
+
+use std::error::Error;
+use std::path::PathBuf;
+use std::env;
+
+use std::str::FromStr;
+
+macro_rules! try_return(
+    ($e:expr) => {{
+        match $e {
+            Ok(v) => v,
+            Err(e) => { println!("Error: {}", e); return; }
+        }
+    }}
+);
+
+#[derive(Debug,PartialEq,Eq,Clone)]
 enum Temperature {
+    Invalid,
     MiliCelcius(i16),
-    ReadError,
-    SensorError,
+    Error(String),
 }
 
 impl Temperature {
@@ -35,7 +63,7 @@ struct Sensor {
 impl Sensor {
     fn new(sensorid: &str) -> Self {
         Sensor {
-            path: format!("/sys/bus/w1/devices/{}/w1_slave", sensorid),
+            path: Self::get_sensor_filename(sensorid),
             crc_regex: Regex::new(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES").unwrap(),
             cap_regex: Regex::new(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)").unwrap(),
         }
@@ -44,44 +72,268 @@ impl Sensor {
     fn read_temp(&self) -> Temperature {
         let f = match File::open(&self.path) {
             Ok(d) => d,
-            Err(_) => return Temperature::ReadError,
+            Err(_) => return Temperature::Error(format!("Couldn't open file {}", &self.path)),
         };
         let f = BufReader::new(f);
 
         let mut lines = f.lines();
 
         // check crc
-        let line = lines.next().unwrap().unwrap();
-        if !self.crc_regex.is_match(&line) {
-            return Temperature::SensorError;
+        if let Some(Ok(line)) = lines.next() {
+            if !self.crc_regex.is_match(&line) {
+                return Temperature::Error(format!("crc failed line=\"{}\"", &line));
+            }
+        } else {
+            return Temperature::Error("crc line couldn't be read".to_owned());
         }
 
-        let line = lines.next().unwrap().unwrap();
-        if let Some(cap) = self.cap_regex.captures(&line) {
-            Temperature::MiliCelcius(cap.at(2).unwrap().parse::<i16>().unwrap())
+        // read temperature
+        if let Some(Ok(line)) = lines.next() {
+            if let Some(cap) = self.cap_regex.captures(&line) {
+                if let Some(temp_string) = cap.at(2) {
+                    match temp_string.parse::<i16>() {
+                        Ok(temp) => Temperature::MiliCelcius(temp),
+                        Err(ref err) => Temperature::Error(format!("Couldn't parse number string=\"{}\" err=\"{}\"", &temp_string, &err)),
+                    }
+                } else {
+                    Temperature::Error(format!("Regular expression value 2 was invalid \"{}\"", &line))
+                }
+            } else {
+                Temperature::Error(format!("Couldn't parse temperature line \"{}\"", &line))
+            }
         } else {
-            Temperature::SensorError
+            return Temperature::Error("temperature line couldn't be read".to_owned());
         }
+    }
+
+    fn get_sensor_filename(sensorid: &str) -> String {
+        format!("{}/{}/w1_slave", Self::get_sensor_base_path(), sensorid)
+    }
+
+    fn get_sensor_base_path() -> String {
+        if cfg!(target_arch = "x86_64") {
+            format!("{}/mnt/sys/bus/w1/devices/", env::home_dir().unwrap().display())
+        } else {
+            "/sys/bus/w1/devices/".to_owned()
+        }
+    }
+
+    fn get_all_sensor_ids() -> Vec<String> {
+        let mut ret = vec![];
+        let paths = std::fs::read_dir(Self::get_sensor_base_path()).unwrap();
+
+        for path in paths {
+            let path = path.unwrap().path().file_name().unwrap().to_str().unwrap().to_owned();
+            if path.starts_with("28-") {
+                ret.push(path);
+            }
+        }
+        ret
     }
 }
 
+struct SensorStore {
+    sensorid: String,
+    data: Arc<RwLock<Vec<(time::Tm, Temperature)>>>,
+    thread: thread::JoinHandle<()>,
+}
+
+impl SensorStore {
+    fn new(sensorid: &str) -> Self {
+        let data = Arc::new(RwLock::new(vec![]));
+        let sensorid = sensorid.to_owned();
+        SensorStore {
+            sensorid: sensorid.to_owned(),
+            data: data.clone(),
+            thread: thread::spawn(move || {
+                let sensor = Sensor::new(&sensorid);
+                let filename = format!("{}.csv", sensorid);
+
+                // load existing data
+                if let Ok(file) = File::open(&filename) {
+                    println!("Read existing data for sensor {}", sensorid);
+                    let file = BufReader::new(file);
+                    let d: Vec<(time::Tm, Temperature)> = file.lines().filter_map(|x| x.ok())
+                                //.inspect(|s| println!("{}", s))
+                                .map(|s| {
+                                    let splitted: Vec<&str> = s.split(",").collect();
+                                    if splitted.len() != 2 {
+                                        println!("skip invalid line line=\"{}\"", s);
+                                        (time::now_utc(), Temperature::Invalid)
+                                    } else {
+                                        if let Ok(time) =time::strptime(&splitted[0], "%Y-%m-%dT%H:%M:%SZ") {
+                                            let temp = match splitted[1].parse::<i16>() {
+                                                Ok(temp) => Temperature::MiliCelcius(temp),
+                                                Err(_) => Temperature::Error(splitted[1].to_owned()),
+                                            };
+                                            (time, temp)
+                                        } else {
+                                            println!("skip line with invalid time line=\"{}\"", s);
+                                            (time::now_utc(), Temperature::Invalid)
+                                        }
+                                    }
+                                })
+                                .filter(|x| match x {
+                                    &(_, Temperature::Invalid) => false,
+                                    _ => true,
+                                })
+                                .collect();
+                    *data.write().unwrap() = d;
+                }
+
+                let mut file = OpenOptions::new().create(true).append(true).open(&filename).unwrap();
+
+                let mut last_temp = Temperature::Invalid;
+                loop {
+                    let current_temp = sensor.read_temp();
+                    let time = time::now_utc();
+                    if current_temp.has_changed(&last_temp) {
+                        data.write().unwrap().push((time, current_temp.clone()));
+                        match current_temp {
+                            Temperature::MiliCelcius(temp) => try_return!(file.write_all(format!("{},{}\n", time.rfc3339(), temp).as_bytes())),
+                            Temperature::Error(ref err) => try_return!(file.write_all(format!("{},{}\n", time.rfc3339(), &err).as_bytes())),
+                            Temperature::Invalid => {},
+                        };
+                        last_temp = current_temp;
+                    }
+                    std::thread::sleep(std::time::Duration::from_secs(5));
+                }
+            }),
+        }
+    }
+
+    fn all() -> BTreeMap<String,Self> {
+        Sensor::get_all_sensor_ids().iter().map(|id| (id.to_owned(), SensorStore::new(&id))).collect()
+    }
+
+    fn as_csv(&self) -> String {
+        let data = self.data.read().unwrap();
+        data.iter().map(|&(time, ref temp)| {
+            match temp {
+                &Temperature::MiliCelcius(ref temp) => format!("{},{}", time.rfc3339(), &temp),
+                &Temperature::Error(ref err) => format!("{},{}", time.rfc3339(), &err),
+                &Temperature::Invalid => format!("{},invalid", time.rfc3339()),
+            }
+        }).collect::<Vec<String>>().join("\n")
+    }
+
+    fn as_csv_from(&self, from: &time::Tm) -> String {
+        let data = self.data.read().unwrap();
+        data.iter().rev()
+            .take_while(|&&(ref time, _)| time > from)
+            .map(|&(time, ref temp)| {
+            match temp {
+                &Temperature::MiliCelcius(ref temp) => format!("{},{}", time.rfc3339(), &temp),
+                &Temperature::Error(ref err) => format!("{},{}", time.rfc3339(), &err),
+                &Temperature::Invalid => format!("{},invalid", time.rfc3339()),
+            }
+        }).collect::<Vec<String>>().into_iter().rev().collect::<Vec<String>>().join("\n")
+    }
+}
+
+struct SenderHandler {
+    sensors: BTreeMap<String, SensorStore>,
+}
+
+impl Handler for SenderHandler {
+    fn handle(&self, req: Request, mut res: Response) {
+        let uri = req.uri.clone();
+        match uri {
+            AbsolutePath(ref path) => match (&req.method, &path[..]) {
+                (&Get, u) => {
+                    let u = match u {
+                        "/" => "/index.html",
+                        o => o,
+                    };
+                    if u == "/api/get/sensors" {
+                        let mut res = try_return!(res.start());
+                        let ret: String = self.sensors.iter().map(|(id,_)| {
+                            id.to_owned()
+                        }).collect::<Vec<String>>().join("\n");
+                        try_return!(res.write_all(ret.as_bytes()));
+                        try_return!(res.end());
+                    } else if u.starts_with("/api/get/temp") {
+                        let mut id: Option<&str> = None;
+                        let mut from: Option<time::Tm> = None;
+                        let splitted: Vec<&str> = u.split("?").collect();
+                        if splitted.len() == 2 {
+                            for query in splitted[1].split("&") {
+                                let sp: Vec<&str> = query.split("=").collect();
+                                if splitted.len() == 2 {
+                                    match sp[0] {
+                                        "id" => id = Some(sp[1]),
+                                        "from" => {
+                                            if let Ok(time) =time::strptime(&sp[1], "%Y-%m-%dT%H:%M:%SZ") {
+                                                from = Some(time);
+                                            }
+                                        },
+                                        _ => {},
+                                    }
+                                }
+                            }
+                        }
+                        if let Some(sensor) = self.sensors.get(id.unwrap_or("")) {
+                            res.headers_mut().set(
+                                ContentType(Mime(TopLevel::Text, SubLevel::Plain,
+                                    vec![(Attr::Charset, Value::Utf8)])));
+                            let mut res = try_return!(res.start());
+                            if let Some(from) = from {
+                                // transfer only what was requested
+                                try_return!(res.write_all(sensor.as_csv_from(&from).as_bytes()));
+                            } else {
+                                // transfer everything
+                                try_return!(res.write_all(sensor.as_csv().as_bytes()));
+                            }
+                            try_return!(res.end());
+                        } else {
+                            *res.status_mut() = hyper::NotFound;
+                        }
+                    } else {
+                        //println!("request: {}", u);
+                        match File::open(format!("content/{}", u)) {
+                            Ok(mut f) => {
+                                let mut res = try_return!(res.start());
+                                try_return!(copy(&mut f, &mut res));
+                                try_return!(res.end());
+                            },
+                            Err(e) => {
+                                println!("url: {} error: {}", u, e);
+                                *res.status_mut() = hyper::NotFound;
+                            },
+                        }
+                    }
+                    return;
+                },
+                (&Post, u) => match u {
+                    "/api/set_alias" => {
+                    },
+                    _ => {
+                        *res.status_mut() = hyper::NotFound;
+                        return;
+                    },
+                },
+                _ => {
+                    *res.status_mut() = hyper::NotFound;
+                    return;
+                }
+            },
+            _ => {
+                return;
+            }
+        };
+    }
+}
 
 fn main() {
-    let sensor = Sensor::new("28-000003a4c40d");
+    println!("Start temprec");
+    let http_handler = SenderHandler {
+        sensors: SensorStore::all(),
+    };
 
-    let mut file = OpenOptions::new().create(true).append(true).open("temperatures.csv").unwrap();
-
-    let mut last_temp = Temperature::ReadError;
-    loop {
-        let current_temp = sensor.read_temp();
-        if current_temp.has_changed(&last_temp) {
-            file.write_all(match current_temp {
-                Temperature::MiliCelcius(temp) => format!("{},{}\n", time::now_utc().rfc3339(), temp),
-                Temperature::ReadError => format!("{},read_error\n", time::now_utc().rfc3339()),
-                Temperature::SensorError => format!("{},sensor_error\n", time::now_utc().rfc3339()),
-            }.as_bytes()).unwrap();
-            last_temp = current_temp;
-        }
-        std::thread::sleep(std::time::Duration::from_secs(5));
+    for (id,_) in &http_handler.sensors {
+        println!(" * {}", id);
     }
+
+    let http_server = Server::http("0.0.0.0:8080").unwrap();
+    http_server.handle(http_handler).unwrap();
 }
